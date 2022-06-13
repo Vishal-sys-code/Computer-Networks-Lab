35e20e81c35f9e0b3beec3972afb62ae386b7c3f
Vishal Pandey <bishalpandey2001@gmail.com>
2022-06-14 01:44:56 +0530

    Create Host_IP_Addess.java

---
+++ Laboratory Sessions/Laboratory 2/Host_IP_Addess.java
@@ -0,0 +1,19 @@
+// Write a program to find the hostname and the ip-address of the local machine
+
+import java.net.*;
+
+class Host_IP_Address{
+    public static void main(String[] args) throws Exception {
+        InetAddress ip = InetAddress.getLocalHost();
+        String host = ip.getHostName();
+        System.out.println("HostName: " + host);
+        System.out.println("IP: " + ip);
+    }
+}
+
+
+/* 
+    Output: 
+        HostName:  1defacb52e61
+        IP Address:  172.28.0.2
+*/ 
