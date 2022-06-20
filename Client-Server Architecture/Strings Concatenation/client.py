309f47c4f2ad32c1d5a37020951c7ce175740cba
Vishal Pandey <bishalpandey2001@gmail.com>
2022-06-20 19:15:28 +0530

    Create client.py

---
+++ Client-Server Architecture/Strings Concatenation/client.py
@@ -0,0 +1,11 @@
+import socket
+from time import sleep
+s=socket.socket()
+s.connect(("localhost",8080))
+
+str1,str2=input("Enter 2 Strings:").split()
+s.send(bytes(str1,'utf-8'))
+sleep(1)
+s.send(bytes(str2,'utf-8'))
+
+print(s.recv(1024).decode())
